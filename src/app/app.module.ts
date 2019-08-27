55bd357c655712c5f015d5b2c4a467a8f710ed17
marianolop22 <marianolop22@yahoo.com.ar>
2019-08-27 16:14:41 -0300

    inicio del proyecto

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SideBarComponent } from './shared/side-bar/side-bar.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LoadingComponent } from './components/loading/loading.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SideBarComponent,
+    NavBarComponent,
+    LoginComponent,
+    LoadingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
